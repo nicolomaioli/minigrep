bc12b3d7776cd2d8a701b8a3e96f8feeaea2cc44
Nicolo Maioli <nicolo@eebria.com>
2019-05-30 09:28:58 +0100

    Unwrap Config or catch and display error and exit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::new(&args);
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
