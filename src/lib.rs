86adc2983b398b993a29a3eedd1a9cfa0659db0f
Nicolo Maioli <nicolomaioli@gmail.com>
2019-05-31 08:38:55 +0100

    Write failing test

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,33 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
 
-    println!("With text:\n\n{}", contents);
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.filename)?;
+
+    println!("{} contents:\n\n{}", &config.filename, &contents);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
 }
