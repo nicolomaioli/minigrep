4e36c2afdb3333b1448a438a3fa8d5f6adfc379b
Nicolo Maioli <nicolo@eebria.com>
2019-05-31 10:10:01 +0100

    Implement search_case_insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,19 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     result
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
 
